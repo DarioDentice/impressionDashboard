912426759577c2303d498780351f1f57eb512bda
dentice <dario.dentice@mooney.it>
2025-10-27 10:32:53 +0100

    feat: add global style and use toggle for enable mock

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,25 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {BrowserRouter} from 'react-router-dom';
 import {FilterProvider} from './context/FilterContext.tsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import App from './App.tsx'
 import './lib/chartjs-setup';
+import {GlobalStyle} from './styles/Global.style';
+
 
 const queryClient = new QueryClient();
 
 async function enableMocking() {
-    if (!import.meta.env.DEV) {
+    const mocksEnabled = import.meta.env.DEV &&
+        localStorage.getItem('enable_mocks') === 'true';
+    if (!mocksEnabled) {
         return;
     }
-    const {worker} = await import('./mocks/browser');
+    console.warn('Mock Service Worker (MSW) is Active.');
 
+    const {worker} = await import('./mocks/browser');
     return worker.start({
         onUnhandledRequest: 'bypass',
     });
@@ -23,12 +28,15 @@ async function enableMocking() {
 enableMocking().then(() => {
     createRoot(document.getElementById('root')!).render(
         <StrictMode>
-            <QueryClientProvider client={queryClient}>
-                <FilterProvider>
-                    <App/>
-                </FilterProvider>
-                <ReactQueryDevtools initialIsOpen={false}/>
-            </QueryClientProvider>
+            <GlobalStyle/>
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <FilterProvider>
+                        <App/>
+                    </FilterProvider>
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                </QueryClientProvider>
+            </BrowserRouter>
         </StrictMode>,
     )
 })
