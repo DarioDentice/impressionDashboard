3333e70a2d9003bbb0f0d0d0537f3027ba35c8a7
dentice <dario.dentice@mooney.it>
2025-10-27 10:28:16 +0100

    feat: rework types

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,22 +1,46 @@
-// Definisce i tipi per il filtro e le risposte API
 export type CountryFilter = 'all' | 'usa' | 'no-usa';
 
-// Risposte API
+export interface Impression {
+    device_id: string;
+    lat: number;
+    lng: number;
+    timestamp: number;
+    country: 'USA' | 'no-usa' | 'not-found';
+    state: string | null;
+}
+
+export interface PaginatedResponse {
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+    data: Impression[];
+}
+
 export interface DeviceStat {
     device_id: string;
     impressions: number;
 }
+
 export interface TimeStat {
     hour?: number;
     day?: number;
     month?: number;
     impressions: number;
 }
+
 export interface StateStat {
     state: string;
     impressions: number;
 }
+
 export interface YearStat {
     year: number;
     impressions: number;
+}
+
+export interface KpiData {
+    totalImpressions: number;
+    dailyChangePercent: number | null;
+    weeklyChangePercent: number | null;
+    topDevice: { id: string; impressions: number } | null;
 }
