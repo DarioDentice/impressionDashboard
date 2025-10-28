8408c8bf93fc9b2de0746a37ff741103819909ca
dentice <dario.dentice@mooney.it>
2025-10-28 15:36:25 +0100

    feat: define queryClient for query Retry control

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,12 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom';
 import {FilterProvider} from './context';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
+import {queryClient} from './lib/queryClient';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import App from './App.tsx'
 import './lib/chartjs-setup';
 import {GlobalStyle} from './styles/Global.style';
-
-
-const queryClient = new QueryClient();
 
 async function enableMocking() {
     const mocksEnabled = import.meta.env.DEV &&
